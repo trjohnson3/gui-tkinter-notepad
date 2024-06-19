080c9966f7163e79e2516e95edd805e151506ee7
Tom Johnson <trjohnson3@gmail.com>
2024-06-19 09:20:25 -0500

    add colors

--- notepad.py
+++ notepad.py
@@ -3,12 +3,15 @@ import tkinter
 #Define window
 root = tkinter.Tk()
 root.title('Notepad')
-root.iconbitmap('./images/notepad')
+root.iconbitmap('./images/notepad.ico')
 root.geometry('600x600')
 root.resizable(0, 0)
 
 #Define fonts and colors
-
+text_color = '#fffacd'
+menu_color = '#dbd9db'
+root_color = '#6c809a'
+root.config(bg= root_color)
 
 #Define functions
 
