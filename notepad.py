ddfa1296ffe5996965d09fbc7b9519d3e0215bfd
Tom Johnson <trjohnson3@gmail.com>
2024-06-18 22:43:16 -0500

    initial setup

---
+++ notepad.py
@@ -0,0 +1,21 @@
+import tkinter
+
+#Define window
+root = tkinter.Tk()
+root.title('Notepad')
+root.iconbitmap('./images/notepad')
+root.geometry('600x600')
+root.resizable(0, 0)
+
+#Define fonts and colors
+
+
+#Define functions
+
+
+#GUI layout
+#Create frames
+
+
+#Run app
+root.mainloop()
